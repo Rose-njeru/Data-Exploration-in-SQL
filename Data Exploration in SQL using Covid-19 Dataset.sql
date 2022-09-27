0b9c9814c973b3f47f0c9bb2b36a277c51f939a7
Rose Njeru <92436079+Rose-njeru@users.noreply.github.com>
2022-09-27 07:03:39 +0300

    Add files via upload
    Commit Changes

--- Data Exploration in SQL using Covid-19 Dataset.sql
+++ Data Exploration in SQL using Covid-19 Dataset.sql
@@ -4,124 +4,125 @@ Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, 
 */
 -- overview of  the Covid-19  data
 
-select *
-from `portfolio project`.`covid vaccinations`;
+SELECT *
+FROM `portfolio project`.`covid vaccinations`;
 
 -- Covid-19 in Kenya
 
 SELECT *
  FROM `portfolio project`.`covid vaccinations`
- where 
+ WHERE
  location ='Kenya' ;
  
  -- Covid-19 in  Africa
  SELECT *
  FROM `portfolio project`.`covid-19 deaths`  
- where continent is not null
- and location like'%Africa%' ;
+ WHERE continent IS NOT NULL
+ AND location LIKE'%Africa%' ;
 
  -- total cases vs population
  
-select location,population,max(total_cases) as highest_infection_count,max(total_cases/population)*100
- as percent_pop_infected
- from `portfolio project`.`covid-19 deaths`
+SELECT location,population,MAX(total_cases)AS highest_infection_count,max(total_cases/population)*100
+AS percent_pop_infected
+ FROM `portfolio project`.`covid-19 deaths`
 -- where location like '%Africa%'
- group by location,population
- order by percent_pop_infected desc; 
+ GROUP BY location,population
+ ORDER BY percent_pop_infected desc; 
  
  -- countries with the highest death count
  
- select location,max(cast(total_deaths as unsigned)) as total_death_count
-from `portfolio project`.`covid-19 deaths`
+ SELECT location,MAX(CAST(total_deaths AS unsigned)) AS total_death_count
+FROM `portfolio project`.`covid-19 deaths`
 -- where location  like '%Africa%'
-where location <> 'High income'
-group by location
-order by total_death_count desc ;
+WHERE location <> 'High income'
+GROUP BY  location
+ORDER BY total_death_count desc ;
  
 -- breakdown by continent
 -- continent with the highest death count
 
- Select continent, MAX(cast(Total_deaths as unsigned)) as TotalDeathCount
-From `portfolio project`.`covid-19 deaths`
+ SELECT continent, MAX(CAST(Total_deaths AS unsigned)) AS TotalDeathCount
+FROM `portfolio project`.`covid-19 deaths`
 -- Where location like '%states%'
-Where continent is not null 
-Group by continent
-order by TotalDeathCount desc;
+WHERE continent IS NOT NULL 
+GROUP BY continent
+ORDER BY TotalDeathCount desc;
 
 --  Global Numbers
 
-select sum(new_cases)as total_cases,sum(cast(new_deaths as unsigned )) as total_deaths,
-sum(cast(new_deaths as unsigned))/sum(new_cases) as death_percentage
-from `portfolio project`.`covid-19 deaths`
+SELECT SUM(new_cases)AS total_cases,SUM(CAST(new_deaths AS unsigned )) AS total_deaths,
+SUM(CAST(new_deaths AS unsigned))/SUM(new_cases) AS death_percentage
+FROM `portfolio project`.`covid-19 deaths`
 -- where location like '%Africa%'
-where  continent is not null
- order by 1,2; 
+WHERE continent IS NOT NULL
+ ORDER BY 1,2; 
  
  -- total cases vs population
 -- Percentage of the population infected
 
- select location,date,population,total_cases,(total_cases/population)*100 as percent_population_infected
-from `portfolio project`.`covid-19 deaths`   
-where location like '%Africa%'
-order by 1,2; 
+ SELECT location,date,population,total_cases,(total_cases/population)*100 AS percent_population_infected
+FROM `portfolio project`.`covid-19 deaths`   
+WHERE location LIKE '%Africa%'
+ORDER BY 1,2; 
 
 -- countries with highest infection rate compared to population
 
-select location, population,date,max(total_cases) as highest_infection_count,
-  max(total_cases/population)*100 as percent_population_infected 
+SELECT location, population,date,MAX(total_cases) AS highest_infection_count,
+  MAX(total_cases/population)*100 AS percent_population_infected 
   FROM `portfolio project`.`covid-19 deaths`    
-  -- where location like'% Africa%'   
-group by location,population,date
-order by percent_population_infected desc;
+  -- WHERE location like'% Africa%'   
+GROUP BY location,population,date
+ORDER BY percent_population_infected DESC;
 
 -- total vaccinations vs populations
 
-select dea.continent, dea.location,dea.date,dea.population,vac.new_vaccinations
-from `portfolio project`.`covid-19 deaths`as dea
- join `portfolio project`.`covid vaccinations` as vac
-on dea.location=vac.location
-and dea.date=vac.date
-where dea.continent is not null
-order by 2,3; 
+SELECT dea.continent, dea.location,dea.date,dea.population,vac.new_vaccinations
+FROM `portfolio project`.`covid-19 deaths`AS dea
+JOIN `portfolio project`.`covid vaccinations` AS vac
+ON dea.location=vac.location
+AND dea.date=vac.date
+WHERE dea.continent IS NOT NULL
+ORDER BY  2,3; 
 
 -- cumulative people vaccinated using Window functions
 
-select dea.continent, dea.location,dea.date,dea.population,
-sum(cast(vac.new_vaccinations as unsigned)) over(partition by dea.location order by dea.location,dea.date
-) as rolling_people_vaccinated
-from `portfolio project`.`covid-19 deaths`as dea
- join `portfolio project`.`covid vaccinations` as vac
-on dea.location=vac.location
-and dea.date=vac.date
--- where continent is not null
-order by 2,3; 
+SELECT dea.continent, dea.location,dea.date,dea.population,
+SUM(CAST(vac.new_vaccinations AS unsigned)) OVER(PARTITION BY dea.location ORDER BY dea.location,dea.date
+) AS rolling_people_vaccinated
+FROM`portfolio project`.`covid-19 deaths`AS dea
+JOIN `portfolio project`.`covid vaccinations` AS vac
+ON dea.location=vac.location
+AND dea.date=vac.date
+-- WHERE continent IS NOT NULL
+ORDER BY 2,3; 
 
 -- rolling people vaccinated vs population using CTE's
 
-with popvsvacc (continent,location,date,population,new_vaccinations,rolling_people_vaccinated)
- as 
-(select dea.continent, dea.location,dea.date,dea.population,vac.new_vaccinations,
-sum(cast(vac.new_vaccinations as unsigned)) over(partition by dea.location order by dea.location,dea.date
-) as rolling_people_vaccinated
-from `portfolio project`.`covid-19 deaths`as dea
- join `portfolio project`.`covid vaccinations` as vac
-on dea.location=vac.location
-and dea.date=vac.date
--- where continent is not null
-order by 2,3)
-select *,(rolling_people_vaccinated/population)*100
-from popvsvacc; 
+WITH popvsvacc (continent,location,date,population,new_vaccinations,rolling_people_vaccinated)
+AS
+(SELECT dea.continent, dea.location,dea.date,dea.population,vac.new_vaccinations,
+SUM(CAST(vac.new_vaccinations AS unsigned)) OVER(PARTITION BY dea.location ORDER BY dea.location,dea.date
+) AS rolling_people_vaccinated
+FROM `portfolio project`.`covid-19 deaths`AS dea
+JOIN`portfolio project`.`covid vaccinations` AS vac
+ON dea.location=vac.location
+AND dea.date=vac.date
+-- WHERE continent IS NOT NULL
+ORDER BY 2,3)
+  
+SELECT *,(rolling_people_vaccinated/population)*100
+FROM popvsvacc; 
 
 -- creating view for later Vizualization
 
-create view percentpopulationvaccinated as
-select  dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations,sum(cast(vac.new_vaccinations as unsigned)) over(partition by dea.location order by dea.location,dea.date) as rolling_people_vaccinated
- FROM `portfolio project`.`covid-19 deaths`as dea
- join `portfolio project`.`covid vaccinations` as vac
- on dea.location=vac.location
- and  dea.date=vac.date
- where dea.continent is not null
-order by 2,3;
+CREATE VIEW  percentpopulationvaccinated AS
+select  dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations,SUM(CAST(vac.new_vaccinations AS unsigned)) over(partition by dea.location order by dea.location,dea.date) as rolling_people_vaccinated
+ FROM `portfolio project`.`covid-19 deaths`AS dea
+JOIN `portfolio project`.`covid vaccinations`ASvac
+ON dea.location=vac.location
+AND  dea.date=vac.date 
+WHERE dea.continent IS NOT NULL
+ORDER BY 2,3;
 
 -- view
 
